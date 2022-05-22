c425dd231c03b8397f77d7fb02b8870d250131c8
Radwa Yassin <radwayassin26@gmail.com>
2022-05-22 22:35:50 +0200

    create home module & components

---
+++ src/app/home/highlights-banner/highlights-banner.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-highlights-banner',
+  templateUrl: './highlights-banner.component.html',
+  styleUrls: ['./highlights-banner.component.scss']
+})
+export class HighlightsBannerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
